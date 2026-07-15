6f6ff468af37dcfdcdae5de948131c46f6d89721
AkshayaKotla <akshayakotla06@gmail.com>
2026-07-15 15:47:11 +0530

    Add files via upload

---
+++ Week-3-SpringRESTusingSpringBoot3/REST - Get country based on country code/src/main/java/com/cognizant/springlearnrest/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.cognizant.springlearnrest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:country.xml")
+public class SpringConfig {
+
+}
