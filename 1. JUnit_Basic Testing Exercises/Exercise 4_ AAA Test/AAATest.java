bc5922d782c13571f82211fec96f62917b03d2dc
AkshayaKotla <akshayakotla06@gmail.com>
2026-07-17 18:10:36 +0530

    Add files via upload

---
+++ 1. JUnit_Basic Testing Exercises/Exercise 4_ AAA Test/AAATest.java
@@ -0,0 +1,38 @@
+package com.cognizant;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AAATest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup executed");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown executed");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(5, result);
+    }
+}
